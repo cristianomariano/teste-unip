0215f4fb06178bd371e822fe4b5f5f604b5bde0a
cristianomariano <cristianomariano@gmail.com>
2023-06-11 20:19:38 -0300

    Update site.js

--- public/site.js
+++ public/site.js
@@ -1,5 +1,9 @@
+function showPassword() {
+  alert("Senha: " + document.getElementById("pass").value);
+}    
+
 function teste() {
-//Instead of x === NaN, use isNaN(x).
+  //Instead of x === NaN, use isNaN(x).
   if (x === NaN) {
     alert("Igual");
   }
